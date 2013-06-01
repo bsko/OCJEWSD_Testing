8a3e3812bf171b291359b82b57524f916b4c136c
bsko <adobeangryslave@gmail.com>
2013-06-01 10:38:59 +0400

    Added DAO instance & SOAP Service

--- src/main/resources/com/myservices/test/rest/EmployeeServiceJSON.java
+++ src/main/resources/com/myservices/test/rest/EmployeeServiceJSON.java
@@ -15,6 +15,8 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.*;
 
+import com.myservices.test.dao.EmployeeDAO;
+import com.myservices.test.dao.exceptions.DAOException;
 import com.myservices.test.entities.Employee;
 import com.myservices.test.entities.UnavailableException;
 
@@ -22,27 +24,17 @@ import com.myservices.test.entities.UnavailableException;
 @Path("json")
 public class EmployeeServiceJSON implements IEmployeeService {
   
+  EmployeeDAO dao = EmployeeDAO.getInstance();
+  
   @Path("/{name}")
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   @Override
   public Employee getEmployee(@PathParam("name") String name) throws UnavailableException {
     try {
-      InputStream is = new FileInputStream(new File(SOURCE_PATH + name + ".properties"));
-      Properties props = new Properties();
-      props.load(is);
-      Employee result = new Employee();
-      result.setId(Long.parseLong(props.getProperty("id")));
-      result.setName(props.getProperty("name"));
-      result.setSurname(props.getProperty("surname"));
-      result.setAge(Integer.parseInt(props.getProperty("age")));
-      result.setPhoneNumber(props.getProperty("phoneNumber"));
-      result.setDescription(props.getProperty("description"));
-      return result;
-    } catch (NumberFormatException e1) {
-      throw new UnavailableException("Information is broken.");
-    } catch (Throwable e) {
-      throw new UnavailableException("No employee with such name found.");
+      return dao.getEmployee(name);
+    } catch (DAOException e) {
+      throw new UnavailableException(e.getMessage());
     }
   }
 
@@ -51,26 +43,11 @@ public class EmployeeServiceJSON implements IEmployeeService {
   @Consumes(MediaType.APPLICATION_JSON)
   @Override
   public Response addEmployee(Employee empl) throws UnavailableException {
-    if(empl.getName() == null || "".equals(empl.getName()) || empl.getId() == null) {
-      throw new UnavailableException("Information is broken.");
-    }
-    Properties props = new Properties();
-    props.setProperty("id", empl.getId().toString());
-    props.setProperty("name", empl.getName());
-    props.setProperty("surname", empl.getSurname());
-    props.setProperty("age", empl.getAge() != null ? empl.getAge().toString() : "");
-    props.setProperty("phoneNumber", empl.getPhoneNumber());
-    props.setProperty("description", empl.getDescription());
-    
-    File file = new File(SOURCE_PATH + empl.getName() + ".properties");
-    if(file.exists()) {
-      file.delete();
-    }
     try {
-      props.store(new FileOutputStream(SOURCE_PATH + empl.getName() + ".properties"), null);
-    } catch(IOException e) {
-      throw new UnavailableException("Couldn't save: " + e.getMessage());
+      dao.addEmployee(empl);
+      return Response.ok("Successfully created!").build();
+    } catch (DAOException e) {
+      throw new UnavailableException(e.getMessage());
     }
-    return Response.ok("Successfully created!").build();
   }
 }
---
+++ target/classes/com/myservices/test/dao/exceptions/DAOException.java
@@ -0,0 +1,14 @@
+package com.myservices.test.dao.exceptions;
+
+public class DAOException extends Exception {
+  
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 406671219516890897L;
+
+  public DAOException(String str) {
+    super(str);
+  }
+  
+}
